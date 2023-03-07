0145cc7067298df8fa7c6aaf29dfb5ad4fdc7eec
Houssam El masoudi <hosam94@live.fr>
2023-03-07 23:10:01 +0100

    animation for mobile too

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 let interval = null;
 
-document.querySelector("h1").onmouseover = event => {  
+document.querySelector("h1").onclick = event => {  
   let iteration = 0;
   
   clearInterval(interval);
@@ -27,12 +27,37 @@ document.querySelector("h1").onmouseover = event => {
   }, 30);
 }
 
+document.querySelector("h1").onmouseover = event => {  
+    let iteration = 0;
+    
+    clearInterval(interval);
+    
+    interval = setInterval(() => {
+      event.target.innerText = event.target.innerText
+        .split("")
+        .map((letter, index) => {
+          if(index < iteration) {
+            return event.target.dataset.value[index];
+          }
+        
+          return letters[Math.floor(Math.random() * 26)]
+        })
+        .join("");
+      
+      if(iteration >= event.target.dataset.value.length){ 
+        clearInterval(interval);
+      }
+      
+      iteration += 1 / 3;
+    }, 30);
+  }
+
 
 (function (d) {
     "use strict";
     const typeText = [
         "Click the links below to contact me!",
-        "Hover over my name to see something cool!",
+        "Click on my name to see something cool!",
         "Check out my Instagram to see cool stuff!",
         "This website is still under construction!",
     ];
